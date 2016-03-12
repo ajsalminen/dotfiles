1827878dd4d9eb5f569c4c3bb4b89fa45d125616
Antti Salminen <antti.salminen@gmail.com>
2016-03-12 22:12:17 +0200

    Add sudo alias to make aliases work with sudo.

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,4 @@
 alias pubip='dig +short myip.opendns.com @resolver1.opendns.com'                                                                                
 alias hpg_shell='links http://hyperpolyglot.org/unix-shells'
+# http://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
+alias sudo='sudo '
