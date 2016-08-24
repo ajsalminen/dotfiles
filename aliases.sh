f11ea9f8554aea8152f9cee88a30e6a477d8e8eb
Antti Salminen <antti.salminen@gmail.com>
2016-08-24 20:12:24 +0300

    Add some new aliases/functions.

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,8 @@
-alias pubip='dig +short myip.opendns.com @resolver1.opendns.com'                                                                                
+alias pubip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias hpg_shell='links http://hyperpolyglot.org/unix-shells'
 # http://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
 alias sudo='sudo '
+
+# https://github.com/anhnk/shell/blob/master/.aliases
+alias week='date +%V'
+alias map="xargs -n1"
