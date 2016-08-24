e7f6d99abd4dd9683ccdce0c401e5907d1155a5a
Antti Salminen <antti.salminen@gmail.com>
2016-08-24 22:14:05 +0300

    wttr.in weather aliases.

--- aliases.sh
+++ aliases.sh
@@ -6,3 +6,6 @@ alias sudo='sudo '
 # https://github.com/anhnk/shell/blob/master/.aliases
 alias week='date +%V'
 alias map="xargs -n1"
+alias wtam='wget --output-document=- --quiet http://wttr.in/tampere'
+alias whel='wget --output-document=- --quiet http://wttr.in/helsinki'
+alias moon='wget --output-document=- --quiet http://wttr.in/moon'
