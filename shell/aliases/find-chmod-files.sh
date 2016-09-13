452c63829fd79c71d3d6a319de99d7cfecdfd7ff
Antti Salminen <antti.salminen@gmail.com>
2016-09-13 16:35:41 +0300

    Function and aliases for changing file/dir permissions with find and a newline fix.

---
+++ shell/aliases/find-chmod-files.sh
@@ -0,0 +1,3 @@
+# Run a recursive chmod for regular files/directories.
+alias fcf='find-chmod f'
+alias fcd='find-chmod d'
---
+++ shell/functions/find-chmod.sh
@@ -0,0 +1,11 @@
+
+# Run find with chmod with given parameters to all files of a type under path.
+# Default to the current directory.
+function find-chmod() {
+    if [ -z "$3" ]; then
+        find_path="."
+    else
+        find_path=$3
+    fi
+    find $find_path -type $1 -exec chmod $@ {} \;
+}
