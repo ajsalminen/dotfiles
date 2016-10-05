edc0374539dae5389531cef72433e241d3308ab4
Antti Salminen <antti.salminen@gmail.com>
2016-10-05 16:02:40 +0300

    Rename find-up function for more shell compatibility.

--- shell/functions/find_up.sh
+++ shell/functions/find_up.sh
@@ -1,6 +1,6 @@
 # Find a file in parent directory.
 # From http://unix.stackexchange.com/questions/6463/find-searching-in-parent-directories-instead-of-subdirectories
-find-up() {
+find_up() {
     path=$(pwd)
     while [[ "$path" != "" && ! -e "$path/$1" ]]; do
         path=${path%/*}
