7046d1418d0fec1cc72c807fe21f7dd3bdd9d7a2
Antti Salminen <antti.salminen@gmail.com>
2017-03-09 02:24:00 +0200

    Line numbers with emacs_create_and_edit.

--- shell/functions/emacs_create_and_edit.sh
+++ shell/functions/emacs_create_and_edit.sh
@@ -1,5 +1,13 @@
 # Make sure it's path exists before opening file for editing in emacs.
 emacs_create_and_edit() {
-    ensure_dir $1
-    emacsclient -n $1
+    ensure_dir "$1"
+
+    if [[ $1 == *":"* ]]; then
+        filename="$(echo "$1" | cut -d ":" -f 1)"
+        line="+$(echo "$1" | cut -d ":" -f 2)"
+    else
+        filename="$1"
+        line=""
+    fi
+    emacsclient -n $line "$filename"
 }
