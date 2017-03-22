dc7cbccc3b98908039d9b9bc8c8ebf8a536ce2ac
Antti Salminen <antti.salminen@gmail.com>
2017-03-23 00:30:15 +0200

    Newer version of duh function.

--- shell/functions/du_sorted.sh
+++ shell/functions/du_sorted.sh
@@ -1,14 +1,15 @@
-# https://gist.github.com/janmoesen/1156154
+# https://gist.github.com/janmoesen/1156154 and
+# https://github.com/janmoesen/tilde/
 # Sort du's output but use human-readable units.
+
 function duh {
-    du -sk "$@" | sort -n;
     du -sk "$@" | sort -n | while read size fname; do
-	for unit in KiB MiB GiB TiB PiB EiB ZiB YiB; do
-	    if [ "$size" -lt 1024 ]; then
-		echo -e "${size}${unit}\t${fname}";
-		break;
-	    fi;
-	    size=$((size/1024));
-	done;
+        for unit in KiB MiB GiB TiB PiB EiB ZiB YiB; do
+            if [ "$size" -lt 1024 ]; then
+                echo -e "${size} ${unit}\t${fname}";
+                break;
+            fi;
+            size=$((size/1024));
+        done;
     done;
 }
