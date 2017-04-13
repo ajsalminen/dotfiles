7200bfeb0adf357c12ddaf81e1d99f40a2bca8ef
Antti Salminen <antti.salminen@gmail.com>
2017-04-13 23:49:06 +0300

    Make sure forwarding is enabled for ansible vagrant function.

--- shell/functions/ansible_vagrant_run.sh
+++ shell/functions/ansible_vagrant_run.sh
@@ -1,7 +1,7 @@
 # Run ansible for vagrant with the right key and playbook from any subdirectory.
 ansible_vagrant_run() {
     # Some of this may not be needed.
-    ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentityFile=~/.vagrant.d/insecure_private_key'
+    ANSIBLE_SSH_ARGS='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o IdentityFile=~/.vagrant.d/insecure_private_key -o ForwardAgent=yes'
     ANSIBLE_HOST_KEY_CHECKING=false
     vagrant_top=$(find_up Vagrantfile)
 
