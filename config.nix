e4767d3e77d986d6a678810124398b42d0e0474e
Antti Salminen <antti.salminen@gmail.com>
2017-09-13 22:14:06 +0300

    Add some missing packages to config.nix.

--- config.nix
+++ config.nix
@@ -1,10 +1,12 @@
 # ~/.nixpkgs/config.nix
 
 {
+  allowBroken = true;
   packageOverrides = pkgs_: with pkgs_; {  # pkgs_ is the original set of packages
     all = with pkgs; buildEnv {  # pkgs is your overriden set of packages itself
       name = "all";
       paths = [
+        python
         emacs
 	git
         go
@@ -12,9 +14,9 @@
         # apparix
         # nq
         # jq
-        python27Packages.ansible_2_1
+        ansible_2_1
         python27Packages.httplib2
-        python36Packages.howdoi
+        python27Packages.howdoi
         php70
         php70Packages.composer
         vagrant
@@ -34,6 +36,13 @@
         mosh
         exa
         ffmpeg
+        pass
+        xclip
+        tomb
+        file
+        python27Packages.upass
+        gnupg1
+        git
       ];
     };
   };
