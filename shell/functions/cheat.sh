4d54177412d02e80e1e62ee961c0be9205430308
Antti J. Salminen <antti.salminen@gmail.com>
2020-04-14 12:14:11 +0300

    Add function for editing cheatsheets and committing the edits to repo.

--- shell/functions/cheat.sh
+++ shell/functions/cheat.sh
@@ -2,3 +2,21 @@
 cheat() {
     command cheat $@ | less
 }
+
+cheat_edit(){
+    cheatsheets_repo=~/repos/cheatsheets
+
+    if [ -d $cheatsheets_repo ]
+    then
+        cd $cheatsheets_repo
+        git pull
+    else
+        git clone git@github.com:ajsalminen/cheatsheets.git ~/repos/cheatsheets
+    fi
+
+    cd $cheatsheets_repo
+    $EDITOR $@
+    git commit -a -m "Updated cheatsheet for $@."
+    git push
+    fresh
+}
